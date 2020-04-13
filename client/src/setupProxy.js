b2bd045d4ab5b0289ae7b3a684c0789da3f9c491
Nguyễn Quang Vinh <31350719+NguyenQuangVinh123@users.noreply.github.com>
2020-04-13 20:40:49 +0700

    Fix error proxy is not a function

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,13 +1,13 @@
-const proxy = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
-    app.use(proxy('/api', { target: 'http://127.0.0.1:3001/',
+    app.use(createProxyMiddleware('/api', { target: 'http://127.0.0.1:3001/',
     headers: {
         "Connection": "keep-alive"
     },
     
     
      }))
-    app.use(proxy('/uploads', { target: 'http://127.0.0.1:3001/',headers: {
+    app.use(createProxyMiddleware('/uploads', { target: 'http://127.0.0.1:3001/',headers: {
         "Connection": "keep-alive"
     }, }))
 
